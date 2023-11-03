f2f3f8c239d2269f1b92a1b2bf96fe2c76b2d15b
Emeka <naemugo@gmail.com>
2023-11-03 10:07:58 +0100

    second

--- codecanyon-33575024-viserbank-digital-banking-system/core/resources/views/templates/basic/layouts/frontend.blade.php
+++
@@ -1,9 +0,0 @@
-@extends($activeTemplate . 'layouts.app')
-@section('main-content')
-    @include($activeTemplate . 'partials.header')
-    <div class="main-wrapper">
-        @include($activeTemplate . 'partials.breadcumb')
-        @yield('content')
-        @include($activeTemplate . 'partials.footer')
-    </div>
-@endsection
