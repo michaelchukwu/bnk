95415d9ec181f4bb87eea64b17af377ae949a0a9
Emeka <naemugo@gmail.com>
2023-11-03 10:05:38 +0100

    first commit

---
+++ codecanyon-33575024-viserbank-digital-banking-system/core/app/Http/Middleware/CheckModule.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class CheckModule {
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next, $name) {
+        if (gs('modules')->$name) {
+            return $next($request);
+        } else {
+            $notify[] = 'Sorry ' . keyToTitle($name) . ' is not available now';
+
+            if ($request->is('api/*')) {
+                return response()->json([
+                    'remark'  => 'module_disable_error',
+                    'status'  => 'error',
+                    'message' => ['error' => $notify],
+                ]);
+            }
+
+            abort(404);
+        }
+    }
+}
